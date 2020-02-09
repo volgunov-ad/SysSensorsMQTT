0ea94d061941faa144c013961804811e40a06a5c
volgunov@gmail.com <545516ae42>
2020-02-09 09:58:16 +0300

    Settings class to separate file

--- sys_sensors_mqtt_daemon.py
+++ sys_sensors_mqtt_daemon.py
@@ -6,10 +6,8 @@ import signal
 import sys
 import time
 
-from pytz import timezone
-import yaml
-
 from sys_sensors_mqtt import MainProcess
+from sys_sensors_settings import Settings
 
 
 class App:
@@ -53,59 +51,6 @@ class App:
             sys.exit()
         else:
             self.logger.warning('SysSensorsMQTT not started')
-
-        
-class Settings(object):
-
-    def __init__(self, logger_obj, file='settings.yaml'):
-        self.settings_file = file
-        self.settings = {}
-        self.logger = logger_obj
-
-    def check_settings(self):
-        if 'mqtt' not in self.settings:
-            self.settings['mqtt'] = {}
-        elif not isinstance(self.settings['mqtt'], dict):
-            self.settings['mqtt'] = {}
-        if 'hostname' not in self.settings['mqtt']:
-            self.settings['mqtt']['hostname'] = '127.0.0.1'
-        if 'port' not in self.settings['mqtt']:
-            self.settings['mqtt']['port'] = 1883
-        else:
-            self.settings['mqtt']['port'] = int(self.settings['mqtt']['port'])
-        if 'user' not in self.settings['mqtt']:
-            self.settings['mqtt']['user'] = ''
-        if 'password' not in self.settings['mqtt']:
-            self.settings['mqtt']['password'] = ''
-        if 'timezone' not in self.settings:
-            self.settings['timezone'] = 'Europe/Moscow'
-        self.settings['timezone'] = timezone(self.settings["timezone"])
-        if 'device_name' not in self.settings:
-            self.settings['device_name'] = 'Device1'
-        if 'client_id' not in self.settings:
-            self.settings['client_id'] = 'client1'
-        if 'model' not in self.settings:
-            self.settings['model'] = 'model'
-        if 'manufacturer' not in self.settings:
-            self.settings['manufacturer'] = 'manufacturer'
-        if 'update_interval' not in self.settings:
-            self.settings['update_interval'] = 300.
-        else:
-            self.settings['update_interval'] = int(self.settings['update_interval'])
-        if 'reboot/shutdown' not in self.settings:
-            self.settings['reboot/shutdown'] = False
-        else:
-            if self.settings['reboot/shutdown'] is not True:
-                self.settings['reboot/shutdown'] = False
-    
-    def read_settings(self):
-        with open('settings.yaml') as f:
-            try:
-                self.settings = yaml.safe_load(f)
-            except yaml.YAMLError:
-                self.logger.info('Some error in settings file')
-                self.settings = {}
-        self.check_settings()
 
 
 def sigterm_handler(_signo, _stack_frame):
